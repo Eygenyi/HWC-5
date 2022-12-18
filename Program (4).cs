96dda6d0b116b7dcac7641bbf493a879df7029bc
Evgeniy <freemen0083@gmail.com>
2022-12-18 16:11:23 +0400

    #4

---
+++ Program (4).cs
@@ -0,0 +1,49 @@
+﻿double[] Arrey(double[] ay)
+{
+    for (int i = 0; i < ay.Length; i++)
+    {
+        ay[i] = new Random().NextDouble()*10;
+    }
+    return ay;
+}
+
+double Max(double[] ar)
+{
+    double max = 0;
+    for (int i = 0; i < ar.Length; i++)
+    {
+
+        if (max < ar[i])
+            max = ar[i];
+    }
+    return Math.Round(max, 3);
+}
+double Min(double[] ar)
+{
+    double min=0;
+    for ( int i = 0; i < ar.Length; i++)
+    { 
+        if (min == 0)
+            min = ar[i];
+            else
+            if (min>ar[i])
+            min = ar[i];
+    }
+    return Math.Round(min, 3);
+}
+void ShowArray(double[] arre)
+{
+    for (int i = 0; i < arre.Length; i++)
+    {
+        Console.Write(Math.Round(arre[i], 3) + " ");
+    }
+}
+Console.WriteLine("Input massive size: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] arr = new double[n];
+double[] egog = Arrey(arr);
+ShowArray(egog);
+double max=Max(egog);
+double min=Min(egog);
+Console.WriteLine();
+Console.Write("max: "+max+" - "+" min: "+min+" = "+Math.Round((max-min),3) );
