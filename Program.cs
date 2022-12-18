711e60360e1e837df53cc6f788ee2ca58517805c
Evgeniy <freemen0083@gmail.com>
2022-12-18 15:38:11 +0400

    #1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int [] Arrey(int [] ay)
+{
+    
+    
+    for (int i = 0; i < ay.Length; i++)
+    {
+        ay[i] = new Random().Next(100,1000);
+    } 
+    return ay;
+}
+
+void ShowArray (int [] arre)
+{
+int temp=0;
+for (int i = 0; i < arre.Length; i++)
+    {
+        
+       Console.Write(arre[i]+" ");
+    if (arre[i]%2==0)
+    temp=temp+1;
+    } 
+Console.WriteLine("Amount of even elements is: " +temp);
+}
+Console.WriteLine("Input massive size: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] arr  = new int [n];
+ShowArray (Arrey(arr));
+
